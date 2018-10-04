a525c01acccfe71b48c5c450baf49557837d5e11
MakovskyiNikolai <makovskyinikolai@gmail.com>
2018-10-04 17:48:39 +0300

    Commit 2 - Input

---
+++ src/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("введите что-нибудь ");
+        //String string = s.nextLine();
+        int x = s.nextInt();
+        System.out.println("вы ввели "+x);
+    }
+}
