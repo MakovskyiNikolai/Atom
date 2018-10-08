9086291dd529e1034ecfc25e4b171da1b31d7842
MakovskyiNikolai <makovskyinikolai@gmail.com>
2018-10-08 15:00:16 +0300

    Commit 4 - Input comments

--- src/Input.java
+++ src/Input.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("введите что-нибудь ");
+        System.out.println("введите какое-нибудь число ");
         //String string = s.nextLine();
         int x = s.nextInt();
         System.out.println("вы ввели "+x);
     }
 }
+// у обьекта класса Scanner вызвали
+// метод nextLine, который читает
+// с клавиатуры 1 строчку.
