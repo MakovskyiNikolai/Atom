0991d2ba6c9fd8ebe91ebbbab3b379bec1493ca0
MakovskyiNikolai <makovskyinikolai@gmail.com>
2018-10-08 15:52:02 +0300

    Commit 5 - DoWhile comments

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class DoWhile {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       int value;
+       do{
+           System.out.println("Введите 5");
+           value = scanner.nextInt();
+       } while(value!=5);
+
+            System.out.println("Вы ввели 5");
+    }
+
+    }
+
+//while - сначала проверяет усовия, а потом проверяет инструкции
+//do while - сначала выполняет инструкции, а потом выполняет уловия
+// - цикл do while гарантирует хотя бы однократное выполнение того, что написано в цикле
+
+//System.out.println("Введите 5");
+//            int value = scanner.nextInt();
+//            while (value!=5) {
+//                System.out.println("Введите 5");
+//                value = scanner.nextInt();
+//            }
