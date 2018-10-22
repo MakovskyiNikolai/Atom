f978253eed5863a065ec706232adead38ab79357
Nikolai <makovskyinikolai@gmail.com>
2018-10-22 13:58:35 +0300

    Commit 14 - Incapsulation - settersAndGetters

--- src/Part_2_ClassesAndObjects.java
+++ src/Part_2_ClassesAndObjects.java
@@ -1,31 +1,44 @@
 public class Part_2_ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.name = "Рома";
-        person1.age = 50;
-        Person person2 = new Person();
-        person2.name = "Павел";
-        person2.age = 20;
-        int year1 = person1.calculateYearsToRetirement();
-        int year2 = person2.calculateYearsToRetirement();
-        System.out.println("Первому человеку до пенсии: "+year1+" лет");
-        System.out.println("Второму человеку до пенсии: "+year2+" лет");
-
+        person1.setName("");
+        person1.setAge(12);
+        System.out.println("Выводим значение в main методе: "+person1.getName());
+        System.out.println("Выводим значение в main методе: "+person1.getAge());
     }
 }
 
 class Person{
-    String name;
-    int age;
+    private String name;  //приват поле - доступно и видно в пределах этого класса (переменная нейм приват)
+    private int age;
 
-    int calculateYearsToRetirement(){
+    public void setName(String userName){
+        if (userName.isEmpty()){
+            System.out.println("Вы ввели пустое имя");
+        } else{
+        name = userName;}
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int userAge){
+        if(userAge<0){
+            System.out.println("Возраст должен быть полодительным");
+        } else{
+        age = userAge;}
+       }
+    public int getAge(){
+        return age;
+}
+
+     int calculateYearsToRetirement(){
         int years = 65-age;
         return years;
     }
 
     void speak(){
         for(int i = 0; i<3; i++){
-        System.out.println("Меня зовут "+ name +", мне "+age+" лет");}
+            System.out.println("Меня зовут "+ name +", мне "+age+" лет");}
     }
     void sayHello(){
         System.out.println("Привет!!!");
@@ -33,6 +46,27 @@ class Person{
 }
 
 
+
+
+
+
+
+
+
+
+
+//часто методу чтобы выполниться необходими дополнительные значения
+
+
+
+
+
+
+
+//у каждого метода есть параметр
+
+
+//1
 
 //Метод - представиться человеку
 
@@ -82,9 +116,77 @@ class Person{
 
 
 
+//2
+//public class Part_2_ClassesAndObjects {
+//    public static void main(String[] args) {
+//        Person person1 = new Person();
+//        person1.name = "Рома";
+//        person1.age = 50;
+//        Person person2 = new Person();
+//        person2.name = "Павел";
+//        person2.age = 20;
+//        int year1 = person1.calculateYearsToRetirement();
+//        int year2 = person2.calculateYearsToRetirement();
+//        System.out.println("Первому человеку до пенсии: "+year1+" лет");
+//        System.out.println("Второму человеку до пенсии: "+year2+" лет");
+//
+//    }
+//}
+//
+//class Person{
+//    String name;
+//    int age;
+//
+//    int calculateYearsToRetirement(){
+//        int years = 65-age;
+//        return years;
+//    }
+//
+//    void speak(){
+//        for(int i = 0; i<3; i++){
+//        System.out.println("Меня зовут "+ name +", мне "+age+" лет");}
+//    }
+//    void sayHello(){
+//        System.out.println("Привет!!!");
+//    }
+//}
 
-
-
+//3
+//public class Part_2_ClassesAndObjects {
+//    public static void main(String[] args) {
+//        Person person1 = new Person();
+//        person1.setNameAndAge ("Роман",20);
+//
+//        String s1 = "Павел";
+//        Person person2 = new Person();
+//        person2.setNameAndAge(s1,30);
+//
+//        person1.speak();
+//        person2.speak();
+//    }
+//}
+//
+//class Person{
+//    String name;
+//    int age;
+//
+//    void setNameAndAge(String username, int userage){
+//        name = username;
+//        age = userage;
+//    }
+//
+//    int calculateYearsToRetirement(){
+//        int years = 65-age;
+//        return years;
+//    }
+//
+//    void speak(){
+//        for(int i = 0; i<3; i++){
+//            System.out.println("Меня зовут "+ name +", мне "+age+" лет");}
+//    }
+//    void sayHello(){
+//        System.out.println("Привет!!!");
+//    }
 
 
 
